bfc43f8de092d0785e234167d19153e565ecffbe
Wellington Santiago <wellingtonhmt2011@gmail.com>
2026-02-03 23:15:20 -0300

    feat: define rotas web iniciais para acesso público, autenticação e áreas protegidas.

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index']);
 
-// Rotas de Autenticação
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth');
+// Rotas para Visitantes (não logados)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store']);
+});
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+// Rotas Protegidas (logados)
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'destroy']);
+    Route::get('/', function () {
+        return view('home');
+    });
+});
