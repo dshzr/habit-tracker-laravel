e648e4f5dfc9c097f216bd30d847f4c19f0afde7
Wellington Santiago <wellingtonhmt2011@gmail.com>
2026-02-03 22:17:56 -0300

    feat: Configura a página inicial do projeto Habit Tracker, atualiza o README e padroniza a indentação de arquivos frontend.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<h1>
-    Olá {{ $name }}!
-</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-<p>
-    <ul>
-        @foreach ($habits as $habit)
-            <li>{{ $habit }}</li>
-        @endforeach
-    </ul> 
-</p>
+<body class="p-12 flex flex-col gap-4">
+    <h1 class="text-5xl">Controle de Hábitos do Well</h1>
+    <p>O controle de hábitos é uma ferramenta que ajuda as pessoas a desenvolverem hábitos saudáveis e a manterem a
+        consistência
+        em suas rotinas.</p>
+
+
+    <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Clique aqui</button>
+</body>
+
+</html>
